7278819c6e900eae3a6a5f6a30ac52ecc044636e
atama526 <100056467+atama526@users.noreply.github.com>
2022-06-09 16:06:18 -0500

    Update utils.js

--- next/utils.js
+++ next/utils.js
@@ -1,12 +1,14 @@
 import web3 from '../contract/web3';
 import React, { useState, useContext, useEffect } from 'react';
 
+//Function to reduce the length of the hash of a Metamask Address only the first 5 characters and the last 4 Characters are displayed
 export const shortenAddress = (address) => `${address.slice(0,5)}...${address.slice(address.length-4)}`; 
 
+//Function to show wei amount in Finney
 export const toFinney = (balance) => web3.utils.fromWei(balance,'finney');
 
 
-
+//Checks if the ethereum wallet is connected, displays message if not
 export const checkIfWalletIsConnected = async () => {
 	if (!window.ethereum) return alert("Please install Metamask");
 	const accounts = await window.ethereum.request({ method: 'eth_accounts'});
