7e574e84afce0da148c1fccc71cb6404b0ff57ed
atama526 <100056467+atama526@users.noreply.github.com>
2022-06-09 16:43:20 -0500

    Update factory.js

--- contract/factory.js
+++ contract/factory.js
@@ -1,6 +1,7 @@
 import web3 from './web3';
 import PayToVote from './PayToVote.json';
 
+//Starts a new instance of the deployed PayToVote contract
 const instance = new web3.eth.Contract(
 	PayToVote.abi,'0xdDFEB0006c91a8a8AC58a3e03a67D72Bc76CD8dC'
 );
