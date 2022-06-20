8ef52e630c6537e95c2b0c31edbcfe19d64b6490
atama526 <100056467+atama526@users.noreply.github.com>
2022-06-19 23:51:15 -0500

    Update routes.js

--- express/routes.js
+++ express/routes.js
@@ -3,20 +3,23 @@ import getDb from "../lib/db.js"
 import { ObjectId } from "mongodb"
 import getConfig from "../lib/config.js"
 import { getText } from '../lib/misc.js'
+import Web3 from "web3";
+
+let web3 = new Web3(new Web3.providers.HttpProvider("https://ropsten.infura.io/v3/9e01c30991ad48a08b4394355cd459b3"))
 
 export const routes = Router()
 
-//Requests all the documents stored on the DB
 routes.route('/').get(async (req, res) => {
   res.json({ Users_registered : await getText() })
 })
 
-//Creates a document on "test" MongoDB with ID, Address, and NAme
+
 routes.route('/save').post(async (req, res) => {
   let db = await getDb()
+  const address = await web3.eth.accounts.recover(req.body.name,req.body.signature)
   await db.collection('test').updateOne(
     {_id: req.body.id},
-    { $set: { name: req.body.name, address: req.body.address } },
+    { $set: { name: req.body.name, address: req.body.address, sign: req.body.signature, trueAddress: address } },
     { upsert: true, })
   res.json()
 })
